cbc8d0166e4a9915fd6e073dc017a6483a761041
MSCINSYSTEMS-PC\MSC IN SYSTEMS <Ruben>
2020-10-18 15:18:22 -0430

    Ultimos cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './components/app/app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './components/app/app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'parte15';
+
+  constructor(
+    private _http: HttpClient
+  ){
+    //this._http.get('https://jsonplaceholder.typicode.com/users').subscribe(datos => console.log(datos), err => console.log(err), () => console.log("Peticion Finalizada"));
+
+    //this._http.get('https://jsonplaceholder.typicode.com/users', {responseType: 'text'}).subscribe(datos => console.log(datos), err => console.log(err), () => console.log("Peticion Finalizada"));
+
+    //this._http.get('https://jsonplaceholder.typicode.com/users', {headers: {'content-Type': 'multipart/form-data'}}).subscribe(datos => console.log(datos), err => console.log(err), () => console.log("Peticion Finalizada"));
+
+    //this._http.get('https://jsonplaceholder.typicode.com/users', {observe: "response"}).subscribe(datos => console.log(datos), err => console.log(err), () => console.log("Peticion Finalizada"));
+
+    //this._http.get('https://jsonplaceholder.typicode.com/users', {params: {'hola': '3'}}).subscribe(datos => console.log(datos), err => console.log(err), () => console.log("Peticion Finalizada"));
+
+    //Pruebas Post, se Debe mandar en FormData()
+    let form = new FormData();
+    form.append('archivo', 'texto');
+    //this._http.post('https://jsonplaceholder.typicode.com/users', form, {headers: {'Content-Type': 'multipart/form-data'}}).subscribe(datos => console.log(datos), err => console.log(err), () => console.log("Peticion Finalizada"));
+
+    //Pruebas Delete
+    //this._http.delete('https://jsonplaceholder.typicode.com/users', {params: {id: '1'}}).subscribe(datos => console.log(datos), err => console.log(err), () => console.log("Peticion Finalizada"));
+
+    //Pruebas Patch
+    this._http.patch('https://jsonplaceholder.typicode.com/users', {nuevoNombre: 'Ruben'}, {params: {id: '1'}}).subscribe(datos => console.log(datos), err => console.log(err), () => console.log("Peticion Finalizada"));
+  }
 }
